b3869d6418b99d7f633b38fff92d254500b6e68e
SkyDynamic <tcplxw@outlook.com>
2025-04-05 22:10:16 +0800

    Add client-side warning messages

--- fabric/src/main/java/io/github/skydynamic/quickbakcupmulti/fabric/client/QuickbakcupmultiReforgedFabricClient.java
+++ fabric/src/main/java/io/github/skydynamic/quickbakcupmulti/fabric/client/QuickbakcupmultiReforgedFabricClient.java
@@ -1,10 +1,11 @@
 package io.github.skydynamic.quickbakcupmulti.fabric.client;
 
+import io.github.skydynamic.quickbakcupmulti.QuickbakcupmultiReforged;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class QuickbakcupmultiReforgedFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        QuickbakcupmultiReforged.logger.warn("QuickbackupmultiReforged mod is not supporting clients now!!!");
     }
 }
---
+++ neoforge/src/main/java/io/github/skydynamic/quickbakcupmulti/neoforge/client/QuickbackupmultiReforgedNeoForgeClient.java
@@ -0,0 +1,12 @@
+package io.github.skydynamic.quickbakcupmulti.neoforge.client;
+
+import io.github.skydynamic.quickbakcupmulti.QuickbakcupmultiReforged;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = QuickbakcupmultiReforged.MOD_ID, dist = Dist.CLIENT)
+public class QuickbackupmultiReforgedNeoForgeClient {
+    public QuickbackupmultiReforgedNeoForgeClient() {
+        QuickbakcupmultiReforged.logger.warn("QuickbackupmultiReforged mod is not supporting clients now!!!");
+    }
+}
