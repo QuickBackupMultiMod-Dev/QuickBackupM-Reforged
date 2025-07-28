02d8e968a7e6fe248fca0e854866617899bf1add
SkyDynamic <tcplxw@outlook.com>
2025-07-28 16:26:41 +0800

    support check update

--- neoforge/src/main/java/io/github/skydynamic/quickbakcupmulti/neoforge/QuickbakcupmultiReforgedNeoForge.java
+++ neoforge/src/main/java/io/github/skydynamic/quickbakcupmulti/neoforge/QuickbakcupmultiReforgedNeoForge.java
@@ -1,11 +1,13 @@
 package io.github.skydynamic.quickbakcupmulti.neoforge;
 
 import io.github.skydynamic.quickbakcupmulti.ModContainer;
+import io.github.skydynamic.quickbakcupmulti.ModVersion;
 import io.github.skydynamic.quickbakcupmulti.QuickbakcupmultiReforged;
 import lombok.Getter;
 import lombok.Setter;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
 
 @Mod(value = QuickbakcupmultiReforged.MOD_ID, dist = Dist.DEDICATED_SERVER)
@@ -17,6 +19,9 @@ public final class QuickbakcupmultiReforgedNeoForge {
 
     public QuickbakcupmultiReforgedNeoForge() {
         modContainer.setConfigPath(FMLPaths.CONFIGDIR.get());
+
+        String version = FMLLoader.getLoadingModList().getModFileById(QuickbakcupmultiReforged.MOD_ID).getMods().getFirst().getVersion().toString();
+        modContainer.setModVersion(new ModVersion(version));
 
         QuickbakcupmultiReforged.init(modContainer);
     }
