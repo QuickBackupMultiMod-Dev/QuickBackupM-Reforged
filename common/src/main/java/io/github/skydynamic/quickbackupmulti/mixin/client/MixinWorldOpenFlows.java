9da38455750eba28d743c4ffa2a73c1c7ce086f9
SkyDynamic <tcplxw@outlook.com>
2026-08-22 16:31:41 +0800

    fix #57

--- common/src/main/java/io/github/skydynamic/quickbackupmulti/mixin/client/MixinWorldOpenFlows.java
+++ common/src/main/java/io/github/skydynamic/quickbackupmulti/mixin/client/MixinWorldOpenFlows.java
@@ -7,6 +7,8 @@ import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
 import net.minecraft.core.LayeredRegistryAccess;
 import net.minecraft.server.RegistryLayer;
 import net.minecraft.server.ReloadableServerResources;
+import net.minecraft.world.level.gamerules.GameRules;
+import net.minecraft.world.level.storage.LevelDataAndDimensions;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Final;
@@ -15,6 +17,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Mixin(WorldOpenFlows.class)
 public class MixinWorldOpenFlows {
@@ -30,11 +34,14 @@ public class MixinWorldOpenFlows {
         )
     )
     private void onCreateLevelFromExistingSettings$createPackRepository(
-        LevelStorageSource.LevelStorageAccess levelStorageAccess, ReloadableServerResources reloadableServerResources,
-        LayeredRegistryAccess<RegistryLayer> layeredRegistryAccess, WorldData worldData,
+        LevelStorageSource.LevelStorageAccess levelSourceAccess,
+        ReloadableServerResources serverResources,
+        LayeredRegistryAccess<RegistryLayer> registryAccess,
+        LevelDataAndDimensions.WorldDataAndGenSettings worldDataAndGenSettings,
+        Optional<GameRules> gameRules,
         CallbackInfo ci
     ) {
-        String worldName = levelStorageAccess.getLevelId();
+        String worldName = levelSourceAccess.getLevelId();
 
         QuickbackupmultiReforged.getModContainer().setLevelId(worldName);
         QuickbackupmultiReforged.setNewDataBase(worldName);
